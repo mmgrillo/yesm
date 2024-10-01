889290154fc3b7af46edec6f4837a4f794902d87
Manuel Monteiro Grillo <mmgrillo1@gmail.com>
2024-10-01 13:25:37 +0100

    app is succesfully fetching transactions through the zerion api

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,39 +1,21 @@
-const config = require('../utils/config');
+const axios = require('axios');
 
-const getApiUrlAndKey = (chain) => {
-  let apiUrl;
-  let apiKey;
+const API_URL = 'https://api.zerion.io/v1';
+const API_KEY = process.env.ZERION_API_KEY; // Fetch from .env file
 
-  switch (chain.toLowerCase()) {
-    case 'ethereum':
-      apiUrl = `https://api.etherscan.io/api`;
-      apiKey = config.etherscanApiKey;
-      break;
-    case 'polygon':
-      apiUrl = `https://api.polygonscan.com/api`;
-      apiKey = config.polygonscanApiKey;
-      break;
-    case 'arbitrum':
-      apiUrl = `https://api.arbiscan.io/api`;
-      apiKey = config.arbitrumscanApiKey;
-      break;
-    case 'optimism':
-      apiUrl = `https://api-optimistic.etherscan.io/api`;
-      apiKey = config.optimismscanApiKey;
-      break;
-    case 'base':
-      apiUrl = `https://api.basescan.org/api`;
-      apiKey = config.basescanApiKey;
-      break;
-    case 'bsc':
-      apiUrl = `https://api.bscscan.com/api`;
-      apiKey = config.bscscanApiKey;
-      break;
-    default:
-      throw new Error(`Chain ${chain} is not supported for fetching transaction details.`);
+class ApiService {
+  static async fetchTransactions(walletAddress) {
+    try {
+      const response = await axios.get(`${API_URL}/wallets/${walletAddress}/transactions`, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`, // Using Bearer token for Zerion API
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch transactions from Zerion: ' + error.message);
+    }
   }
+}
 
-  return { apiUrl, apiKey };
-};
-
-module.exports = { getApiUrlAndKey };
+module.exports = ApiService;
